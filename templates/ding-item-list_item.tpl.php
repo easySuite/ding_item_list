400a72faeedaca16dca7a2b66ec5c6c25c221d87
AnatolyMuntean <am@inlead.dk>
2012-06-07 15:26:28 +0400

    case 566 - Block elements and some structure changes. Reserve button.

--- templates/ding-item-list_item.tpl.php
+++ templates/ding-item-list_item.tpl.php
@@ -11,10 +11,12 @@
     <img src="<?php print $cover; ?>" alt="" />
   </div>
   <div class="item-details">
-    <p class="item-title"><?php print $title; ?></p>
-    <p class="item-author"><?php print t('By');?> <?php print $author; ?> (<?php print $year;?>)</p>
-    <p class="item-rating"><?php print $rating; ?> (<?php print $rating_count; ?>)</p>
-    <p class="item-reviews">(<?php print $review_count;?>) <?php print t('reviews'); ?></p>
-    <p class="item-loan"></p>
+    <div class="item-title"><?php print $title; ?></div>
+    <div class="item-author"><?php print t('By');?> <?php print $author; ?> (<?php print $year;?>)</div>
+    <div class="item-rating">
+      <div class="rating-value-<?php print $rating; ?>"><?php print $rating; ?></div>
+      <span class="rating-count">(<?php print $rating_count; ?>)</span></div>
+    <div class="item-reviews">(<?php print $review_count;?>) <?php print t('reviews'); ?></div>
+    <div class="item-loan"><?php print $loan_form; ?></div>
   </div>
 </div>
