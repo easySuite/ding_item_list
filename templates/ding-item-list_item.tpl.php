72cfd9a69fdbc55a20b05d91de90c035f7521be2
Vadim Malai <mv@inlead.dk>
2012-09-21 16:34:05 +0300

    case 1173 - Item list should have a graceful check for VoxB.

--- templates/ding-item-list_item.tpl.php
+++ templates/ding-item-list_item.tpl.php
@@ -15,10 +15,12 @@
   <div class="item-details">
     <div class="item-title"><a href="/ting/object/<?php print $faust; ?>"><?php print $title; ?></a></div>
     <div class="item-author"><?php print t('By');?> <?php print $author; ?> (<?php print $year;?>)</div>
+    <?php if(function_exists('ding_voxb_menu')) : ?>
     <div class="item-rating">
       <div class="rating-value-<?php print $rating; ?>"><?php print $rating; ?></div>
       <span class="rating-count">(<?php print $rating_count; ?>)</span></div>
     <div class="item-reviews">(<?php print $review_count;?>) <?php print t('reviews'); ?></div>
+    <?php endif ?>
     <div class="item-loan"><?php print $loan_form; ?></div>
   </div>
 </div>
