0b8f8ceeb963a22a10073f56c4a3571b8359073f
Nicolae Grigoriu <ng@inlead.dk>
2018-10-31 16:17:50 +0200

    EASYOPAC-990 - Adjust objects search to the new method.

--- templates/ding-item-list_item.tpl.php
+++ templates/ding-item-list_item.tpl.php
@@ -13,14 +13,9 @@ $alt = check_plain($title . ' ' . $author);
   </div>
   <div class="item-details">
     <div class="item-title"><a href="/ting/object/<?php print $faust; ?>"><?php print $title; ?></a></div>
-    <div class="item-author"><?php if (!empty($author)) { print t('By @author', array('@author' => $author)); } ?> (<?php print $year;?>)</div>
-    <?php if ($has_rating): ?>
-      <div class="item-rating">
-        <div class="rating-value-<?php print $rating; ?>"><?php print $rating; ?></div>
-        <span class="rating-count">(<?php print $rating_count; ?>)</span>
-      </div>
-      <div class="item-reviews">(<?php print $review_count;?>) <?php print t('reviews'); ?></div>
-    <?php endif; ?>
+      <?php if (!empty($author)): ?>
+        <div class="item-author"><?php print t('By @author', array('@author' => $author)); ?> (<?php print $year;?>)</div>
+      <?php endif; ?>
       <?php if (!empty($loan_form)): ?>
         <div class="item-loan"><?php print $loan_form; ?></div>
       <?php endif; ?>
